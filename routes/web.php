bff0cc5e71f97ecef202db012ab46beeabe90904
lovevisss <530822987@qq.com>
2016-12-17 08:20:59 -0500

    add tables Course Time and Room

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,21 @@ Route::get('/all', function(){
 });
 
 Route::get('/git', function(){
-	return 'git is up';
+	Excel::load(public_path('course.xls'), function($reader){
+		$results = $reader->get();
+		$a_re = $results->first()->toArray();
+		foreach ($a_re as $key => $value) {
+			echo "id:".$value['id']."name:".$value['name'];
+		}
+		return $results;
+	});
 });
 
 Route::get('/test', function() {
     return 'need to install laravel excel';
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+Route::get('/addcourse', 'CourseController@create_course');
+Route::get('/addtime', 'TimeController@create_time');
